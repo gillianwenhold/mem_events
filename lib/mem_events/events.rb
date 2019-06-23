91eebdd6acee795773fbd243e93a246a389d599f
gillianwenhold <gwenhold@mac.com>
2019-06-23 12:57:00 -0400

    added initialize and test data to events class

--- lib/mem_events/events.rb
+++ lib/mem_events/events.rb
@@ -1,20 +1,35 @@
 class MemEvents::Events
 
+  attr_accessor :all
+
+  @@all = []
+
+  def initialize(name, time)
+    @name = name
+    @time = time
+    @@all << self
+  end
 
   def self.list
+    array = [
+      ["The Awakening 'Paint with a Twist'", "Sun June 23rd 3:00pm - 5:00pm"],
+      ["2019 Craft Food & Wine Festival", "Sun June 23rd 4:00pm - 9:00pm"],
+      ["Train/Goo Goo Dolls", "Sun June 23rd 7:00pm - 10:00pm"],
+      ["Garden Camp", "Mon Jun 24th 9:00am - Fri 28th 12:00pm"],
+      ["D is for Dinosaur Dig", "Mon Jun 24th 9:00am - Fri 28th 12:00pm"],
+      ["If I had a Camera | Art Shay: Activism, Civil Rights & Justice", "Mon June 24th 9:00am - Sat Oct 5th 5:00pm"],
+      ["Free Socializing Event with DJ AO", "tbd"],
+      ["Next Event 1", "tbd"],
+      ["Next Event 2", "tbd"],
+      ["Next Event 3", "tbd"],
+      ["Next Event 4", "tbd"]
+    ]
 
-    puts <<-DOC
-    {name: "The Awakening 'Paint with a Twist'", time: "Sun June 23rd 3:00pm - 5:00pm"},
-    {name: "2019 Craft Food & Wine Festival", time: "Sun June 23rd 4:00pm - 9:00pm"},
-    {name: "Train/Goo Goo Dolls", time: "Sun June 23rd 7:00pm - 10:00pm"},
-    {name: "Garden Camp", time: "Mon Jun 24th 9:00am - Fri 28th 12:00pm"},
-    {name: "D is for Dinosaur Dig", time: "Mon Jun 24th 9:00am - Fri 28th 12:00pm"},
-    {name: "If I had a Camera | Art Shay: Activism, Civil Rights & Justice", time: "Mon June 24th 9:00am - Sat Oct 5th 5:00pm"},
-    {name: "Free Socializing Event with DJ AO", time: "tbd"},
-    {name: "Next Event 1", time: "tbd"},
-    {name: "Next Event 2", time: "tbd"}
-    DOC
-
+    array.each do |item|
+      title = item[0]
+      date = item[1]
+      self.new(title, date)
+    end
   end
 
 end
