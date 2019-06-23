ad8a362348440a3d74d9dc0c92cd4146d9cbee93
gillianwenhold <gwenhold@mac.com>
2019-06-23 12:24:41 -0400

    added notes file and built out CLI to include more eventsss

--- lib/mem_events/cli.rb
+++ lib/mem_events/cli.rb
@@ -6,12 +6,12 @@ class MemEvents::CLI
 
   def call
     puts "Events in Memphis"
+    list_events
     find_events
-    more_info
     goodbye
   end
 
-  def find_events
+  def list_events
     puts <<-DOC
     1. The Awakening 'Paint with a Twist - Sun June 23rd 3:00pm - 5:00pm
     2. 2019 Craft Food & Wine Festival - Sun June 23rd 4:00pm - 9:00 pm
@@ -19,7 +19,7 @@ class MemEvents::CLI
     DOC
   end
 
-  def more_info
+  def find_events
     puts "Enter the number of an event you'd like more information about, or enter 'more' to see more events! Or type 'exit' to quit."
     input = ""
     while input != "exit"
@@ -36,12 +36,20 @@ class MemEvents::CLI
         puts "More info on third event"
         puts "Enter the number of another event, 'more' to see more, or 'exit' to quit!"
       when "more"
-        puts "More events"
-        puts "Enter the number of an event to see details, 'more' to see more, or 'exit' to quit!"
+        more_events
       else
         puts "Not sure what you're asking for...try again!"
       end
     end
+  end
+
+  def more_events
+    puts <<-DOC
+    1. Next event 1
+    2. Next event 2
+    3. Next event 3
+    DOC
+    find_events
   end
 
   def goodbye
---
+++ notes.md
@@ -0,0 +1,19 @@
+CLI:
+  Prints a list of 5(?) events in Memphis
+  Gives the event name and time of event
+  Asks user which event to learn more about
+    Can also type more to see the next events
+    Can also type exit to quit the program
+  Goes to detail page of the event
+
+Events:
+  Event name
+  Event date/time
+  Event details url
+
+Details:
+  Event name
+  Event date/time
+  Event location
+  Event summary
+  Link to FB event
